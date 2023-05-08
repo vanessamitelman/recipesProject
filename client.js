781b9a228aeed2499cda0b117a2e99c83b039197
Vanessa Mitelman <vanessamitelman@gmail.com>
2023-05-08 17:46:33 +0300

    my first commit

--- client.js
+++ client.js
@@ -2,3 +2,22 @@ const client = contentful.createClient({
   space: 'arw5tmqsoy9e',
   accessToken: '8rJFvhxfbfqsNT6H48NKSjq64AcBd5tnopzvSNsSNcE'
 });
+const getCarouselSlides = async () => {
+  try {
+    const response = await client.getEntries({
+      content_type: 'kitchenCarousel'
+    });
+    const responseData = response.items;
+    console.log(cleanData(responseData));
+  } catch (error) {
+    console.log(error);
+  }
+};
+const cleanData = (rawData) => {
+  return rawData.map((item) => {
+    const { fields } = item;
+    return fields;
+  });
+};
+
+getCarouselSlides();
